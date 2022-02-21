f9c7faf6b47032fb60ae9060d8beb72524d05500
Hai Lee <duonghailee@gmail.com>
2022-02-21 15:49:08 +0200

    Improve code format

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-from elg.model import TextsResponseObject
 import unittest
 import json
 import requests
+
 
 class TestResponseStucture(unittest.TestCase):
     base_url = 'http://localhost:8001/process'
@@ -10,7 +10,7 @@ class TestResponseStucture(unittest.TestCase):
             ' English sentence\nBoris Johnson left London\nThis is second sentence in'\
             ' English\nOlen asunnut Suomessa noin 10 vuotta'
 
-    params = {"includeOrig": "True","languageSet":["fin","swe","eng"]}
+    params = {"includeOrig": "True", "languageSet": ["fin", "swe", "eng"]}
 
     payload = json.dumps({
         "type": "text",
@@ -19,33 +19,37 @@ class TestResponseStucture(unittest.TestCase):
     })
 
     headers = {
-    'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
     }
 
     def test_api_response_status_code(self):
         """Should return status code 200
         """
 
-        status_code = requests.post(self.base_url, headers=self.headers, data=self.payload).status_code
+        status_code = requests.post(
+            self.base_url, headers=self.headers, data=self.payload).status_code
         self.assertEqual(status_code, 200)
 
     def test_valid_api_response(self):
         """Return response should not be empty
         """
 
-        response = requests.post(self.base_url, headers=self.headers, data=self.payload).json()['response']
+        response = requests.post(
+            self.base_url, headers=self.headers, data=self.payload).json()['response']
         self.assertNotEqual(response, None)
 
     def test_api_response_type(self):
         """Should return ELG annotation response type
         """
-        response = requests.post(self.base_url, headers=self.headers, data=self.payload).json()['response']
+        response = requests.post(
+            self.base_url, headers=self.headers, data=self.payload).json()['response']
         self.assertEqual(response.get('type'), 'annotations')
 
     def test_api_response_content(self):
         """Annotation response should contain only three keys fin,swe,eng
         """
-        response = requests.post(self.base_url, headers=self.headers, data=self.payload).json()['response']
+        response = requests.post(
+            self.base_url, headers=self.headers, data=self.payload).json()['response']
         self.assertEqual(len(response['annotations'].keys()), 3)
         for lang in self.params['languageSet']:
             self.assertIn(lang, response['annotations'].keys())
@@ -54,17 +58,18 @@ class TestResponseStucture(unittest.TestCase):
         """Annotation response of Finnish input sentence should
         contain three objects with corresponding correct offsets
         """
-        response = requests.post(self.base_url, headers=self.headers, data=self.payload).json()['response']
+        response = requests.post(
+            self.base_url, headers=self.headers, data=self.payload).json()['response']
         fin_result = response['annotations']['fin']
         self.assertEqual(len(fin_result), 3)
         print(fin_result)
 
         for result in fin_result:
-            self.assertEqual(result['start'], self.texts.find(result['features']['original_text']))
-            self.assertEqual(result['end'], result['start'] + len(result['features']['original_text']))
+            self.assertEqual(result['start'], self.texts.find(
+                result['features']['original_text']))
+            self.assertEqual(result['end'], result['start'] +
+                             len(result['features']['original_text']))
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
