4fb823edf33452f32725996451efc9e0cbf13e73
Hai Lee <duonghailee@gmail.com>
2022-02-22 10:32:24 +0200

    Clean code according to flake8 convention

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-
-
 import os
 from elg import FlaskService
 from elg.model import Failure, TextRequest, AnnotationsResponse
 from elg.model.base import StandardMessages
-
 
 from subprocess import Popen, PIPE
 import threading
@@ -15,114 +12,130 @@ from collections import defaultdict
 import languagecodes
 
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
 
 class LidHeli(FlaskService):
 
-  # Remove control characters from a string:
-  def remove_control_characters(self, s):
-      return "".join(ch for ch in s if unicodedata.category(ch)[0] != "C")
+    # Remove control characters from a string:
+    def remove_control_characters(self, s):
+        return "".join(ch for ch in s if unicodedata.category(ch)[0] != "C")
 
-  def process_text(self, request: TextRequest):
+    def process_text(self, request: TextRequest):
 
-      global language_set, n_best_lang
-      # handle some params
-      langmap, orig = None, None
-      params = request.params
-      if params:
-        orig = params.get('includeOrig', False)
-        language_set = params.get('languageSet', None)
-        langmap = params.get('languageMap', None)
-        n_best_lang = params.get('bestLangs', 10)
+        global language_set, n_best_lang
+        # handle some params
+        langmap, orig = None, None
+        params = request.params
+        if params:
+            orig = params.get("includeOrig", False)
+            language_set = params.get("languageSet", None)
+            langmap = params.get("languageMap", None)
+            n_best_lang = params.get("bestLangs", 10)
 
-      texts = request.content
-      output = defaultdict(list)
-      offset = 0
-      with lid_lock:
-        for id, l in enumerate(texts.split('\n')):
-            in_text = self.remove_control_characters(l.replace("\r", "").replace(
-                "\n", " ").replace("\t", " ").replace("\u2028", " ").replace("\u2029", " ")) + "\n"
-            process.stdin.write(in_text.encode("utf-8"))
-            process.stdin.flush()
-            lang_list = []
-            # read output until there's an empty line:
-            while True:
-                raw_line = process.stdout.readline().decode("utf-8").strip("\n")
-                if len(raw_line) and raw_line != "xxx":
-                    m = re.search("^\[(.*)\],([0-9\.]+)", raw_line)
-                    if m:
-                        langs = m.group(1).split(", ")
-                        score = m.group(2)
-                        for l3 in langs:
-                            l2 = languagecodes.iso_639_alpha2(l3)
+        texts = request.content
+        output = defaultdict(list)
+        offset = 0
+        with lid_lock:
+            for id, l in enumerate(texts.split("\n")):
+                in_text = (self.remove_control_characters(
+                    l.replace("\r", "").replace("\n", " ").replace(
+                        "\t", " ").replace("\u2028", " ").replace(
+                            "\u2029", " ")) + "\n")
+                process.stdin.write(in_text.encode("utf-8"))
+                process.stdin.flush()
+                lang_list = []
+                # read output until there's an empty line:
+                while True:
+                    raw_line = process.stdout.readline().decode("utf-8").strip(
+                        "\n")
+                    if len(raw_line) and raw_line != "xxx":
+                        m = re.search(r"^\[(.*)\],([0-9\.]+)", raw_line)
+                        if m:
+                            langs = m.group(1).split(", ")
+                            score = m.group(2)
+                            for l3 in langs:
+                                l2 = languagecodes.iso_639_alpha2(l3)
 
-                            if langmap:
-                                if l2 in langmap and isinstance(langmap[l2], str):
-                                    l2 = langmap[l2]
-                                    l3 = languagecodes.iso_639_alpha3(l2)
+                                if langmap:
+                                    if l2 in langmap and isinstance(
+                                            langmap[l2], str):
+                                        l2 = langmap[l2]
+                                        l3 = languagecodes.iso_639_alpha3(l2)
 
-                            lang_list.append(
-                                {"lang2": l2, "lang3": l3, "score": float(score)})
-                else:  # end of output
-                    break
+                                lang_list.append({
+                                    "lang2": l2,
+                                    "lang3": l3,
+                                    "score": float(score)
+                                })
+                    else:  # end of output
+                        break
 
-            lang2 = None
-            lang3 = None
-            conf = 0
+                lang2 = None
+                lang3 = None
+                conf = 0
 
-            if len(lang_list):
-                lang2 = lang_list[0]["lang2"]
-                lang3 = lang_list[0]["lang3"]
-                # difference of log-probabilities between two best-scoring languages
-                if len(lang_list) > 1:
-                    conf = lang_list[1]["score"] - lang_list[0]["score"]
+                if len(lang_list):
+                    lang2 = lang_list[0]["lang2"]
+                    lang3 = lang_list[0]["lang3"]
+                    # difference of log-probabilities
+                    # between two best-scoring languages
+                    if len(lang_list) > 1:
+                        conf = lang_list[1]["score"] - lang_list[0]["score"]
 
-            if language_set:
-                # default to first language in language_set:
-                if len(language_set):
-                    lang3 = language_set[0]
-                    lang2 = languagecodes.iso_639_alpha3(lang3)
+                if language_set:
+                    # default to first language in language_set:
+                    if len(language_set):
+                        lang3 = language_set[0]
+                        lang2 = languagecodes.iso_639_alpha3(lang3)
 
-                    # pick the best returned language that is in language_set:
-                    found = False
-                    for i in range(len(lang_list)):
-                        lg = lang_list[i]
-                        if lg["lang3"] in language_set:
-                            found = True
-                            lang2 = lg["lang2"]
-                            lang3 = lg["lang3"]
-                            conf = lg["score"]
-                            # if i > 0:  # not picking the best language => output zero confidence
-                            #     conf = 0  # lang_list[0]["score"]-lg["score"]
-                            break
-                    if not found:
+                        # pick the best returned language
+                        #  that is in language_set:
+                        found = False
+                        for i in range(len(lang_list)):
+                            lg = lang_list[i]
+                            if lg["lang3"] in language_set:
+                                found = True
+                                lang2 = lg["lang2"]
+                                lang3 = lg["lang3"]
+                                conf = lg["score"]
+                                # if not picking the best language
+                                # => output zero confidence
+                                if i > 0:
+                                    conf = 0  # lang_list[0]["score"]-lg["score"]
+                                break
+                        if not found:
+                            conf = 0
+
+                    # empty language_set => None
+                    else:
+                        lang2 = None
+                        lang3 = None
                         conf = 0
 
-                # empty language_set => None
-                else:
-                    lang2 = None
-                    lang3 = None
-                    conf = 0
+                clf_obj = {
+                    "start": offset,
+                    "end": offset + len(l),
+                    "features": {
+                        "lang3": lang3,
+                        "lang2": lang2,
+                        "confidence": conf
+                    },
+                }
+                offset = offset + len(l) + 1
 
-            clf_obj = {
-                "start": offset,
-                "end": offset + len(l),
-                "features": {"lang3": lang3, "lang2": lang2, "confidence": conf}
-            }
-            offset = offset + len(l) + 1
+                if orig:
+                    clf_obj["features"]["original_text"] = l
 
-            if orig:
-              clf_obj["features"]["original_text"] = l
-
-            output[lang3].append(clf_obj)
-      try:
-        return AnnotationsResponse(annotations=output)
-      except Exception as e:
-        detail = {'server error': str(e)}
-        error = StandardMessages.generate_elg_service_internalerror(
-            detail=detail)
-        return Failure(errors=[error])
+                output[lang3].append(clf_obj)
+        try:
+            return AnnotationsResponse(annotations=output)
+        except Exception as e:
+            detail = {"server error": str(e)}
+            error = StandardMessages.generate_elg_service_internalerror(
+                detail=detail)
+            return Failure(errors=[error])
 
 
 lid_heli_service = LidHeli("lid-heli-service")
@@ -134,7 +147,7 @@ lid_lock = threading.Lock()
 n_best_lang = 10
 language_set = None
 
-inDev = os.getenv('FLASK_ENV')
+inDev = os.getenv("FLASK_ENV")
 
 
 @app.before_first_request
@@ -143,16 +156,57 @@ def setup():
     app.logger.info("before_first_request")
 
     if inDev:
-      if language_set:
-          process = Popen(['java', '-jar', 'HeLI.jar', '-l', ",".join(language_set),
-                          '-t', str(n_best_lang)], stdin=PIPE, stdout=PIPE)
-      else:
-          process = Popen(['java', '-jar', 'HeLI.jar', '-t',
-                          str(n_best_lang)], stdin=PIPE, stdout=PIPE)
+        if language_set:
+            process = Popen(
+                [
+                    "java",
+                    "-jar",
+                    "HeLI.jar",
+                    "-l",
+                    ",".join(language_set),
+                    "-t",
+                    str(n_best_lang),
+                ],
+                stdin=PIPE,
+                stdout=PIPE,
+            )
+        else:
+            process = Popen(
+                ["java", "-jar", "HeLI.jar", "-t",
+                 str(n_best_lang)],
+                stdin=PIPE,
+                stdout=PIPE,
+            )
     else:  # in Docker
-      if language_set:
-          process = Popen(['/java/bin/java', '-XX:+UseG1GC', '-Xms2g', '-Xmx2g', '-jar', 'HeLI.jar', '-l', ",".join(language_set),
-                          '-t', str(n_best_lang)], stdin=PIPE, stdout=PIPE)
-      else:
-          process = Popen(['/java/bin/java', '-XX:+UseG1GC', '-Xms2g', '-Xmx2g', '-jar', 'HeLI.jar', '-t',
-                          str(n_best_lang)], stdin=PIPE, stdout=PIPE)
+        if language_set:
+            process = Popen(
+                [
+                    "/java/bin/java",
+                    "-XX:+UseG1GC",
+                    "-Xms2g",
+                    "-Xmx2g",
+                    "-jar",
+                    "HeLI.jar",
+                    "-l",
+                    ",".join(language_set),
+                    "-t",
+                    str(n_best_lang),
+                ],
+                stdin=PIPE,
+                stdout=PIPE,
+            )
+        else:
+            process = Popen(
+                [
+                    "/java/bin/java",
+                    "-XX:+UseG1GC",
+                    "-Xms2g",
+                    "-Xmx2g",
+                    "-jar",
+                    "HeLI.jar",
+                    "-t",
+                    str(n_best_lang),
+                ],
+                stdin=PIPE,
+                stdout=PIPE,
+            )
--- test.py
+++ test.py
@@ -6,50 +6,50 @@ import requests
 class TestResponseStucture(unittest.TestCase):
     base_url = 'http://localhost:8001/process'
 
-    texts = 'Suomi on kaunis maa\nMitä tänään syötäisiin?\nGod morgon!\nThis is an'\
-            ' English sentence\nBoris Johnson left London\nThis is second sentence in'\
+    texts = 'Suomi on kaunis maa\nMitä tänään syötäisiin?\
+            \nGod morgon!\nThis is an'\
+            ' English sentence\nBoris Johnson left London\
+            \nThis is second sentence in'\
             ' English\nOlen asunnut Suomessa noin 10 vuotta'
 
     params = {"includeOrig": "True", "languageSet": ["fin", "swe", "eng"]}
 
-    payload = json.dumps({
-        "type": "text",
-        "params": params,
-        "content": texts
-    })
+    payload = json.dumps({"type": "text", "params": params, "content": texts})
 
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    headers = {'Content-Type': 'application/json'}
 
     def test_api_response_status_code(self):
         """Should return status code 200
         """
 
-        status_code = requests.post(
-            self.base_url, headers=self.headers, data=self.payload).status_code
+        status_code = requests.post(self.base_url,
+                                    headers=self.headers,
+                                    data=self.payload).status_code
         self.assertEqual(status_code, 200)
 
     def test_valid_api_response(self):
         """Return response should not be empty
         """
 
-        response = requests.post(
-            self.base_url, headers=self.headers, data=self.payload).json()['response']
+        response = requests.post(self.base_url,
+                                 headers=self.headers,
+                                 data=self.payload).json()['response']
         self.assertNotEqual(response, None)
 
     def test_api_response_type(self):
         """Should return ELG annotation response type
         """
-        response = requests.post(
-            self.base_url, headers=self.headers, data=self.payload).json()['response']
+        response = requests.post(self.base_url,
+                                 headers=self.headers,
+                                 data=self.payload).json()['response']
         self.assertEqual(response.get('type'), 'annotations')
 
     def test_api_response_content(self):
         """Annotation response should contain only three keys fin,swe,eng
         """
-        response = requests.post(
-            self.base_url, headers=self.headers, data=self.payload).json()['response']
+        response = requests.post(self.base_url,
+                                 headers=self.headers,
+                                 data=self.payload).json()['response']
         self.assertEqual(len(response['annotations'].keys()), 3)
         for lang in self.params['languageSet']:
             self.assertIn(lang, response['annotations'].keys())
@@ -58,17 +58,19 @@ class TestResponseStucture(unittest.TestCase):
         """Annotation response of Finnish input sentence should
         contain three objects with corresponding correct offsets
         """
-        response = requests.post(
-            self.base_url, headers=self.headers, data=self.payload).json()['response']
+        response = requests.post(self.base_url,
+                                 headers=self.headers,
+                                 data=self.payload).json()['response']
         fin_result = response['annotations']['fin']
         self.assertEqual(len(fin_result), 3)
-        print(fin_result)
 
         for result in fin_result:
-            self.assertEqual(result['start'], self.texts.find(
-                result['features']['original_text']))
-            self.assertEqual(result['end'], result['start'] +
-                             len(result['features']['original_text']))
+            self.assertEqual(
+                result['start'],
+                self.texts.find(result['features']['original_text']))
+            self.assertEqual(
+                result['end'],
+                result['start'] + len(result['features']['original_text']))
 
 
 if __name__ == '__main__':
